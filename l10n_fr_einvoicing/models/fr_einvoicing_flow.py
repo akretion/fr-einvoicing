070c779ce174af2434719ddc43400875dafd185d
Alexis de Lattre <alexis.delattre@akretion.com>
2026-08-08 17:40:38 +0200

    [IMP] l10n_fr_einvoicing: add info logs for out flows
    Improve display_name of flows: add syntax
    
    (cherry picked from commit b7df7dac669f030720ac79fd2b89cebc108e1ef9)

--- l10n_fr_einvoicing/models/fr_einvoicing_flow.py
+++ l10n_fr_einvoicing/models/fr_einvoicing_flow.py
@@ -187,7 +187,9 @@ class FrEinvoicingFlow(models.Model):
         for flow in self:
             name = flow.identifier
             if not name:
-                name = self.env._("ID %s: to send", flow.id)
+                name = self.env._("ID %s", flow.id)
+            if flow.syntax:
+                name = f"{name} {flow.syntax}"
             if flow.state:
                 name = f"{name} ({state2label.get(flow.state)})"
             flow.display_name = name
@@ -312,6 +314,11 @@ class FrEinvoicingFlow(models.Model):
             "filename": filename,
         }
         self.sudo().write(vals)
+        msg = (
+            f"Flow {self.display_name} ID {self.id} successfully generated "
+            f"in syntax {self.syntax}"
+        )
+        log_obj._info_log(result, msg)
         if "updated_count" in result:
             result["updated_count"] += 1
 
@@ -451,6 +458,8 @@ class FrEinvoicingFlow(models.Model):
             "odoo_error_details": False,
         }
         self.sudo().write(flow_vals)
+        msg = f"Flow {self.display_name} ID {self.id} successfully sent"
+        log_obj._info_log(result, msg)
         if "updated_count" in result:
             result["updated_count"] += 1
         if self.move_ids:
@@ -1036,9 +1045,10 @@ class FrEinvoicingFlow(models.Model):
             self.sudo().write(vals)
         if vals.get("state"):
             msg = (
-                f"Successful status update of flow {self.display_name} ID {self.id}. "
-                f"New state: '{vals['state']}'"
+                f"Successful status update of flow {self.display_name} ID {self.id}: "
+                f"new state is '{vals['state']}'"
             )
+            log_obj._info_log(result, msg)
             if "updated_count" in result:
                 result["updated_count"] += 1
 
